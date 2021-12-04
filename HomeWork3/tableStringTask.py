6389a863d415df6091ff37824936710ad74b2f49
viktor-198 <vinitskiyv@gmail.com>
2021-12-04 18:16:17 +0200

    Example. Using string methods

--- HomeWork3/tableStringTask.py
+++ HomeWork3/tableStringTask.py
@@ -6,3 +6,7 @@ x = x.capitalize()
 y = y.replace('R', 'А', 1)
 z = z.replace('DdD', 'Сергеевич')
 print(x, y, z)
+
+
+full_name = " кузнецов Rлексей DdD"
+print(full_name.strip().lower().replace('r', 'А').replace('ddd', 'Сергеевич').title())
